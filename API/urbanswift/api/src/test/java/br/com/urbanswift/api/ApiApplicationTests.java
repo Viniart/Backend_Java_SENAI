289f54c62682c7b7c9db9ea837877a67bac07c94
Vinicio <seu@email.com>
2025-09-12 16:56:46 -0300

    :bug: fix: Consertando projeto Entregas - falta de arquivo main

---
+++ API/urbanswift/api/src/test/java/br/com/urbanswift/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.urbanswift.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
