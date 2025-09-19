bdd2ad6219c1be6f26bf2a15764bef21a1b2902f
Vinicio <seu@email.com>
2025-09-19 16:24:26 -0300

    :wastebasket: remove: Removendo projeto duplicado para manter organização

--- API/techmarket-api/src/main/java/br/com/techmarket/techmarket_api/exception/BusinessException.java
+++
@@ -1,7 +0,0 @@
-package br.com.techmarket.techmarket_api.exception;
-
-public class BusinessException extends RuntimeException {
-    public BusinessException(String message) {
-        super(message);
-    }
-}
--- API/techmarket-api/src/main/java/br/com/techmarket/techmarket_api/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package br.com.techmarket.techmarket_api.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
